b597bdab56389bfa62ccf8aba195d19f118b3cf0
francis <francis@DESKTOP-OE02C04>
2024-03-17 09:13:59 +1100

    implemented a singleton to handle in-memory saving of customers

---
+++ CustomersApp/CustomersApp.Infrastructure/Persistence/CustomerContext.cs
@@ -0,0 +1,30 @@
+﻿using CustomersApp.Domain.Entities;
+using System.Collections.ObjectModel;
+
+namespace CustomersApp.Infrastructure.Persistence
+{
+    internal class CustomerContext
+    {
+        private static CustomerContext instance = null;
+        private static readonly object instanceLock = new object();
+
+        private List<Customer> customers = new List<Customer>();
+        public ReadOnlyCollection<Customer> Customers { get { return customers.AsReadOnly(); } }
+
+        private CustomerContext() { }
+        public static CustomerContext Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new CustomerContext();
+                    }
+                    return instance;
+                }
+            }
+        }
+    }
+}
