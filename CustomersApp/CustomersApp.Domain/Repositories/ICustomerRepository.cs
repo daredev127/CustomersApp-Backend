b70f897753912db5314a4c1d1541162b218afaf6
francis <francis@DESKTOP-OE02C04>
2024-03-17 09:11:08 +1100

    added the customer repository interface

---
+++ CustomersApp/CustomersApp.Domain/Repositories/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using CustomersApp.Domain.Entities;
+
+namespace CustomersApp.Domain.Repositories
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer> GetCustomerById(Guid Id);
+        Task<IEnumerable<Customer>> GetAllCustomers();
+        Task<Customer> AddNewCustomer(Customer newCustomer);
+    }
+}
